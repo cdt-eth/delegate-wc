ebf24e2212fe17008e9e2d57b6d3ce1ae68db311
christianturner <christianturner@hey.com>
2023-08-30 16:16:25 -0400

    rename file

---
+++ src/lib/ConnectButton.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+</script>
+
+<button
+  class="rounded-lg border border-transparent px-4 py-3 text-base font-semibold bg-dg-gray cursor-pointer transition-border-color duration-200 hover:border-dg-blue focus:outline-none"
+>
+  Connect
+</button>
