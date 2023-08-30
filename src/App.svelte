b1c73bac28af11a632493875e414e3dc49c19fef
christianturner <christianturner@hey.com>
2023-08-30 16:16:40 -0400

    ui updates

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,30 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg';
-  import viteLogo from '/vite.svg';
-  import Counter from './lib/Counter.svelte';
+  import delegateLogo from './assets/logo.svg';
+  import ConnectButton from './lib/ConnectButton.svelte';
+  // import { WagmiConfig, createConfig } from 'wagmi';
 </script>
 
-<main>
+<main class="w-2/3 m-auto flex flex-col gap-8">
+  <div class="p-2 flex items-center m-auto w-[300px] justify-center gap-4">
+    <a class="cursor-default p-2" href="https://delegate.xyz/" target="_blank" rel="noreferrer">
+      <img
+        id="logo"
+        class="logo h-24 p-6 transition duration-300 m-auto cursor-pointer"
+        src={delegateLogo}
+        alt="Delegate Logo"
+        style="padding: 1.5em;"
+      />
+    </a>
+
+    <ConnectButton />
+  </div>
+
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+    <p class="text-4xl">Secure your onchain identity</p>
+
+    <p class="text-[#888] pt-3 m-auto">
+      Keep your vaulted NFTs and ERC20s safe by linking wallets together. Claim airdrops, prove
+      token ownership, and more from your hot wallet.
+    </p>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer"
-      >SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
