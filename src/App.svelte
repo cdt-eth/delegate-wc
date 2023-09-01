4113810abd5e7ea81a20c32e2ed7e5096b7ad974
christianturner <christianturner@hey.com>
2023-08-31 23:24:02 -0400

    connect & disconnect functionality plus styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,129 @@
 <script lang="ts">
   import delegateLogo from './assets/logo.svg';
+  import { walletStore } from './lib/stores/WalletStore';
   import ConnectButton from './lib/ConnectButton.svelte';
-  // import { WagmiConfig, createConfig } from 'wagmi';
+  import {
+    InjectedConnector,
+    configureChains,
+    createConfig,
+    getNetwork,
+    getPublicClient,
+  } from '@wagmi/core';
+  import { WalletConnectConnector } from '@wagmi/connectors/walletConnect';
+  import { publicProvider } from '@wagmi/core/providers/public';
+  import { infuraProvider } from '@wagmi/core/providers/infura';
+  import {
+    type Chain,
+    mainnet,
+    goerli,
+    sepolia,
+    arbitrum,
+    arbitrumGoerli,
+    avalanche,
+    avalancheFuji,
+    base,
+    baseGoerli,
+    bsc,
+    bscTestnet,
+    canto,
+    celo,
+    celoAlfajores,
+    fantom,
+    fantomTestnet,
+    gnosis,
+    gnosisChiado,
+    klaytn,
+    optimism,
+    optimismGoerli,
+    polygon,
+    polygonMumbai,
+    zora,
+    zoraTestnet,
+  } from '@wagmi/core/chains';
+  import { onMount } from 'svelte';
+  import { getAccount } from '@wagmi/core';
+
+  const { chains, publicClient, webSocketPublicClient } = configureChains(
+    [
+      mainnet,
+      goerli,
+      sepolia,
+      arbitrum,
+      arbitrumGoerli,
+      avalanche,
+      avalancheFuji,
+      base,
+      baseGoerli,
+      bsc,
+      bscTestnet,
+      canto,
+      celo,
+      celoAlfajores,
+      fantom,
+      fantomTestnet,
+      gnosis,
+      gnosisChiado,
+      klaytn,
+      optimism,
+      optimismGoerli,
+      polygon,
+      polygonMumbai,
+      zora,
+      zoraTestnet,
+    ],
+    [infuraProvider({ apiKey: import.meta.env.VITE_INFURA_PROJECT_ID }), publicProvider()],
+  );
+
+  const config = createConfig({
+    autoConnect: true,
+    publicClient,
+    webSocketPublicClient,
+    connectors: [
+      new InjectedConnector({
+        chains,
+        options: {
+          name: 'Metamask',
+        },
+      }),
+      new WalletConnectConnector({
+        options: {
+          projectId: import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID,
+          showQrModal: true,
+        },
+      }),
+    ],
+  });
+
+  $: address = $walletStore.address;
+  $: status = $walletStore.status;
+
+  const account = getAccount();
+  const { chain } = getNetwork();
+
+  onMount(() => {
+    if (account.isConnected) {
+      walletStore.set({
+        address: account.address as `0x${string}`,
+        chain: chain?.name,
+        status: 'connected',
+        // provider:
+      });
+    } else {
+      walletStore.set({
+        address: null,
+        chain: chain?.name,
+        status: 'disconnected',
+        // provider:
+      });
+    }
+  });
+
+  const trimEthAddress = (address: string) =>
+    [address.slice(0, 5), address.slice(address.length - 4)].join('...');
 </script>
 
-<main class="w-2/3 m-auto flex flex-col gap-8">
-  <div class="p-2 flex items-center m-auto w-[300px] justify-center gap-4">
+<main class="w-full m-auto flex flex-col gap-8">
+  <div class="p-2 flex items-center m-auto w-full justify-center gap-4">
     <a class="cursor-default p-2" href="https://delegate.xyz/" target="_blank" rel="noreferrer">
       <img
         id="logo"
@@ -16,15 +134,25 @@
       />
     </a>
 
-    <ConnectButton />
+    <ConnectButton {config} />
   </div>
 
-  <div>
-    <p class="text-4xl">Secure your onchain identity</p>
+  {#if status === 'connected'}
+    <div>
+      <p class="text-3xl">Welcome, {address && trimEthAddress(address)}!</p>
 
-    <p class="text-[#888] pt-3 m-auto">
-      Keep your vaulted NFTs and ERC20s safe by linking wallets together. Claim airdrops, prove
-      token ownership, and more from your hot wallet.
-    </p>
-  </div>
+      <p class="text-[#888] pt-3 m-auto text-xl italic">
+        You are {status} to {$walletStore.chain}.
+      </p>
+    </div>
+  {:else}
+    <div>
+      <p class="text-4xl">Secure your onchain identity</p>
+
+      <p class="text-[#888] pt-3 m-auto">
+        Keep your vaulted NFTs and ERC20s safe by linking wallets together. Claim airdrops, prove
+        token ownership, and more from you r hot wallet.
+      </p>
+    </div>
+  {/if}
 </main>
--- src/lib/ConnectButton.svelte
+++ src/lib/ConnectButton.svelte
@@ -1,8 +1,115 @@
 <script lang="ts">
+  import Modal from './Modal.svelte';
+  import { walletStore, disconnectWallet } from './stores/WalletStore';
+  import metamaskLogo from '../assets/metamask.svg';
+  import walletConnectLogo from '../assets/walletConnect.png';
+  import xButton from '../assets/xButton.svg';
+  import {
+    connect,
+    type Config,
+    type PublicClient,
+    type WebSocketPublicClient,
+    getAccount,
+  } from '@wagmi/core';
+  import type { FallbackTransport } from 'viem';
+
+  export let config: Config<
+    PublicClient<FallbackTransport>,
+    WebSocketPublicClient<FallbackTransport>
+  >;
+
+  let showModal = false;
+
+  function openModal() {
+    showModal = true;
+  }
+
+  function closeModal() {
+    showModal = false;
+  }
+
+  function getWalletIcon(index: number) {
+    switch (index) {
+      case 0:
+        return metamaskLogo;
+      case 1:
+        return walletConnectLogo;
+      default:
+        return walletConnectLogo;
+    }
+  }
+
+  async function connectWallet(index: number) {
+    try {
+      const result = await connect({
+        connector: config.connectors[index],
+      });
+      console.log('Connected successfully:', result);
+
+      walletStore.set({
+        address: result.account,
+        chain: result.connector?.chains[0].name,
+        status: 'connected',
+        // provider: result.provider,
+      });
+      console.log('Connected walletStore:', walletStore);
+
+      closeModal();
+    } catch (error) {
+      console.error('Error connecting:', error);
+    }
+  }
+
+  async function handleClick() {
+    if (status !== 'disconnected') {
+      disconnectWallet();
+    } else {
+      openModal();
+    }
+  }
+
+  $: status = $walletStore.status;
 </script>
 
 <button
-  class="rounded-lg border border-transparent px-4 py-3 text-base font-semibold bg-dg-gray cursor-pointer transition-border-color duration-200 hover:border-dg-blue focus:outline-none"
+  on:click={handleClick}
+  class="rounded-lg border border-transparent px-4 py-3 text-base font-semibold bg-[#383838] hover:bg-white hover:bg-opacity-10 cursor-pointer transition-border-color duration-200 focus:outline-none"
 >
-  Connect
+  {#if status === 'connected'}
+    Disconnect
+  {:else}
+    Connect
+  {/if}
 </button>
+
+<Modal {showModal} on:close={closeModal}>
+  <div class="text-white">
+    <div class="flex items-center justify-between mb-6">
+      <div class="w-3" />
+      <h2 class="text-xl font-semibold">Connect Wallet</h2>
+
+      <button
+        class="text-white w-6 h-6 cursor-pointer transform transition-transform duration-300 hover:bg-[#333333] hover:rounded-full"
+        on:click={closeModal}
+      >
+        <img class="w-3 h-3 m-auto" src={xButton} alt="x-button" />
+      </button>
+    </div>
+
+    <!-- Connectors -->
+    <div class="flex flex-col gap-4">
+      {#each [...config.connectors] as connector, index}
+        <button
+          class="bg-[#383838] hover:bg-white hover:bg-opacity-10 text-white px-3 py-2 rounded-2xl"
+          on:click={() => connectWallet(index)}
+        >
+          <div class="flex justify-between items-center p-2">
+            <p class="font-semibold text-lg">{connector.name}</p>
+
+            <img class="h-8 w-8" src={getWalletIcon(index)} alt={connector.name} />
+          </div>
+        </button>
+      {/each}
+    </div>
+  </div>
+</Modal>
